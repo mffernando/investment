61e2ff42aa3ce7fb7cda9d0672bdb1a1af9ba9d8
Fernando <fmti91@hotmail.com>
2018-04-30 17:12:17 -0300

    new entity Moviment (php artisan make:entity Moviment) and migrate (php artisan migrate).

---
+++ app/Entities/Moviment.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Prettus\Repository\Contracts\Transformable;
+use Prettus\Repository\Traits\TransformableTrait;
+
+/**
+ * Class Moviment.
+ *
+ * @package namespace App\Entities;
+ */
+class Moviment extends Model implements Transformable
+{
+    use TransformableTrait;
+
+    protected $fillable = ['user_id', 'group_id', 'product_id', 'value', 'type'];
+
+    public function user(){
+      return $this->belongsTo(User::class);
+    }
+
+    public function group(){
+      return $this->belongsTo(Group::class);
+    }
+
+    public function product(){
+      return $this->belongsTo(Product::class);
+    }
+}
